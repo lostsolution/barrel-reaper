0188d1e090ee235a067f4b1c2b07989b1c22a281
Edvin CANDON <edvin.candon@proton.ch>
2025-09-15 20:44:47 +0200

    Prepare bin output

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 2024,
-      sourceType: "module",
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2024,
+            sourceType: 'module',
+        },
+        rules: {
+            'no-console': 'off',
+            curly: ['error', 'multi-line'],
+        },
     },
-    rules: {
-      "no-console": "off",
-      curly: ["error", "multi-line"],
-    },
-  },
 ]);
